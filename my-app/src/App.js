8667957efd1cbbd42e94775dd5c6fc6e6d81b484
John Long <johnlonggately@gmail.com>
2021-09-17 10:47:36 +0000

    Solution for challenge 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import css from './App.module.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <Sidebar />
     </div>
   );
 }
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar
