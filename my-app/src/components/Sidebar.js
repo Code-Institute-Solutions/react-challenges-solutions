fc445a6dee54016e106c8aaaa221300d5a34028c
John Long <johnlonggately@gmail.com>
2021-09-17 11:03:06 +0000

    Solution for challenge 2

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import css from "./css/Sidebar.module.css";
 
 function Sidebar() {
     return (
-        <div>
+        <div className={css.sidebar}>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
