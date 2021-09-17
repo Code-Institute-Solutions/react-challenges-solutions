7083bf1d2af025ab507e7792eeb82b036e4b9d5e
John Long <johnlonggately@gmail.com>
2021-09-17 13:20:52 +0000

    Solution to challenge 6

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,22 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
     
     render() {
@@ -17,24 +29,11 @@ export class Content extends Component {
                 </div>
 
                 <div className={css.SearchResults}>
-
-                    {/* Part 1: Creating the map function */}
-
-                        {/* {
-                    savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                                </div>
-                        })
-                    } */}
-
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
         )
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./css/Loader.module.css";
+
+function Loader() {
+    return (
+        <div className={css.Loader}>
+            
+        </div>
+    )
+}
+
+export default Loader
